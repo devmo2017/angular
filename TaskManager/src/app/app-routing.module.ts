dbcad9e50928d66314d89fba149971d0ce88ef5c
Mo Ri <devmo2017@gmail.com>
2020-02-09 22:53:43 -0800

    updated route for home page to dashboard

--- TaskManager/src/app/app-routing.module.ts
+++ TaskManager/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AboutComponent } from "./about/about.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "about", component: AboutComponent },
+  { path: "", redirectTo: "dashboard", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
