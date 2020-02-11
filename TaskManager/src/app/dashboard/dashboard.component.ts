2bd4a6a2cf357f16121bb0402d02187d332807d3
Mo Ri <devmo2017@gmail.com>
2020-02-10 23:54:11 -0800

    added various gui elements

--- TaskManager/src/app/dashboard/dashboard.component.ts
+++ TaskManager/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  Designation: string;
+  Username: string;
+  NoOfTeamMembers: number;
+  TotalCostOfAllProjects: number;
+  PendingTasks: number;
+  UpComingProjects: number;
+  ProjectCost: number;
+  CurrentExpenditure: number;
+  AvailableFunds: number;
 
   ngOnInit(): void {
+    this.Designation = "Team Leader";
+    this.Username = "John Smith";
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 21135087;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
   }
-
 }
