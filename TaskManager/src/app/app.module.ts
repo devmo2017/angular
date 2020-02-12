5c9911a8f1ca078f84b41972d3a9d6397cf4010f
Mo Ri <devmo2017@gmail.com>
2020-02-12 14:14:07 -0800

    refactored to add service

--- TaskManager/src/app/app.module.ts
+++ TaskManager/src/app/app.module.ts
@@ -1,22 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AdminModule } from "./admin/admin.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AdminModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
